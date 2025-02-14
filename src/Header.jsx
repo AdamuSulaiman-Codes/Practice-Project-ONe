4216da6bd37713319dd90c1e09ce987b8f10efc6
Adamu Sulaiman Muhammed <sulaimancodes2235@gmail.com>
2025-02-14 02:42:39 -0800

    created the file

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div id='header'>
+        <img src="" alt="" />
+        <h1>React Investment Calculator</h1>
+    </div>
+  )
+}
+
+export default Header
